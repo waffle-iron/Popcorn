882fc59c20113d71d75ecd1c5505294c3fb34368
Benjamin Bougot <bbougot@gmail.com>
2015-09-08 15:07:49 +0200

    Improve tests
    Former-commit-id: 55fd10816ee686137f5163842e0c0952960b4826

--- Popcorn.Tests/Comparers/MovieFullComparerTest.cs
+++ Popcorn.Tests/Comparers/MovieFullComparerTest.cs
@@ -20,36 +20,74 @@ namespace Popcorn.Tests.Comparers
         public void Equals_SameMovie_ReturnsTrue()
         {
             var fixture = new Fixture();
-            var movie = fixture.Create<MovieFull>();
+
+            var movie1 = fixture.Create<MovieFull>();
+            var movie2 = fixture.Create<MovieFull>();
+
+            var id = fixture.Create<int>();
+            var dateUploadedUnix = fixture.Create<int>();
+
+            movie1.Id = id;
+            movie2.Id = id;
+            movie1.DateUploadedUnix = dateUploadedUnix;
+            movie2.DateUploadedUnix = dateUploadedUnix;
+
             Assert.Equals(
-                _comparer.Equals(movie, movie), true);
+                _comparer.Equals(movie1, movie1), true);
+
+            Assert.Equals(
+                _comparer.Equals(movie1, movie2), true);
         }
 
         [Test]
         public void Equals_DifferentMovies_ReturnsFalse()
         {
             var fixture = new Fixture();
+
             var movie1 = fixture.Create<MovieFull>();
             var movie2 = fixture.Create<MovieFull>();
+
             Assert.Equals(
                 _comparer.Equals(movie1, movie2), false);
+
+            Assert.Equals(
+                _comparer.Equals(movie1, null), false);
+
+            Assert.Equals(
+                _comparer.Equals(movie2, null), false);
         }
 
         [Test]
         public void GetHashCode_SameMovie_ReturnsSameHashCode()
         {
             var fixture = new Fixture();
-            var movie = fixture.Create<MovieFull>();
+
+            var movie1 = fixture.Create<MovieFull>();
+            var movie2 = fixture.Create<MovieFull>();
+
+            var id = fixture.Create<int>();
+            var dateUploadedUnix = fixture.Create<int>();
+
+            movie1.Id = id;
+            movie2.Id = id;
+            movie1.DateUploadedUnix = dateUploadedUnix;
+            movie2.DateUploadedUnix = dateUploadedUnix;
+
             Assert.Equals(
-                _comparer.GetHashCode(movie), _comparer.GetHashCode(movie));
+                _comparer.GetHashCode(movie1), _comparer.GetHashCode(movie1));
+
+            Assert.Equals(
+                _comparer.GetHashCode(movie1), _comparer.GetHashCode(movie2));
         }
 
         [Test]
         public void GetHashCode_DifferentMovies_ReturnsDifferentHashCode()
         {
             var fixture = new Fixture();
+
             var movie1 = fixture.Create<MovieFull>();
             var movie2 = fixture.Create<MovieFull>();
+
             Assert.AreNotEqual(
                 _comparer.GetHashCode(movie1), _comparer.GetHashCode(movie2));
         }
