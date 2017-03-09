5eb3fb660406a6027ace7d5953a2be75c6b1abcf
Benjamin Bougot <bbougot@gmail.com>
2017-03-09 23:29:02 +0100

    Improve UI

--- Popcorn/UserControls/Home/Movie/Details/MovieDetailsUserControl.xaml.cs
+++ Popcorn/UserControls/Home/Movie/Details/MovieDetailsUserControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Popcorn.UserControls.Home.Movie.Details
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Popcorn.UserControls.Home.Movie.Details
 {
     /// <summary>
     /// Interaction logic for Movie.xaml
@@ -11,6 +15,30 @@
         public MovieDetailsUserControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Each time the mouse wheel has been changed, report the event to the parent
+        /// </summary>
+        /// <param name="sender">The page</param>
+        /// <param name="e">MouseWheelEventArgs args</param>
+        private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+                {
+                    RoutedEvent = MouseWheelEvent,
+                    Source = sender
+                };
+
+                var parent = ((Control)sender).Parent as UIElement;
+                if (parent == null)
+                    return;
+
+                parent.RaiseEvent(eventArg);
+            }
         }
     }
 }
